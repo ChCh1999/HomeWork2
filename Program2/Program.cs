69823f99199edf812ab07b8b4f11580e7c7cb7fb
ChCh1999 <34148951+ChCh1999@users.noreply.github.com>
2018-09-15 11:21:41 +0800

    Finished

--- Program2/Program.cs
+++ Program2/Program.cs
@@ -6,7 +6,43 @@ namespace Program2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("输入数组的元素个数：");
+            int nCount = int.Parse(Console.ReadLine());
+
+            //初始化数组
+            int[] NumArray;
+            int SumOfArrey;
+            int MaxNum;
+            int MinNum;
+            double averageOfNum;
+            SumOfArrey = 0;
+            NumArray = new int[nCount];
+
+            //输入数组
+            Console.WriteLine("输入一串整数:");
+            for (int i = 0; i < nCount; i++)
+            {
+                NumArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            //处理数据
+            MinNum = NumArray[0];
+            MaxNum = NumArray[0];
+            for (int i = 0; i < nCount; i++)
+            {
+                SumOfArrey += NumArray[i];
+                if (NumArray[i] > MaxNum)
+                    MaxNum = NumArray[i];
+                if (NumArray[i] < MinNum)
+                    MinNum = NumArray[i];
+            }
+            averageOfNum = SumOfArrey / nCount;
+
+            //输出数据
+            Console.WriteLine("数组的最大值为： " + MaxNum + ";");
+            Console.WriteLine("数组的最小值为： " + MinNum + ";");
+            Console.WriteLine("数组的平均值为： " + averageOfNum + ";");
+            Console.WriteLine("数组的和为： " + SumOfArrey + ";");
         }
     }
 }
---
+++ Program3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+namespace Program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //初始化2~100的数组
+            int[] AllNum;
+            AllNum = new int[99];
+            for(int i = 0; i < 99; i++)
+            {
+                AllNum[i] = i + 2;
+            }
+
+            //筛选
+            for(int i = 2; i < 100; i++)
+            {
+                for(int j = 0; j < 99; j++)
+                {
+                    if (AllNum[j] % i == 0&&AllNum[j]!=i)
+                        AllNum[j] = 0;
+                }
+                
+            }
+
+            //输出100以内的素数
+            foreach(int i in AllNum)
+            {
+                if (i != 0)
+                    Console.Write(i + " 、");
+            }
+        }
+    }
+}
