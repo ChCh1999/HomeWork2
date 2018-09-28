b12263a340093ad9d93c4d56b10713f8939acd23
ChCh1999 <34148951+ChCh1999@users.noreply.github.com>
2018-09-28 18:55:42 +0800

    homework1 update
    添加第一次作业

---
+++ HomeWork1/Program2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        double[] num;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String Num1 = textBox1.Text;
+            double num1 = double.Parse(Num1);
+            String Num2 = textBox2.Text;
+            double num2 = double.Parse(Num2);
+            double Result = num1 * num2;
+            result.Text = Num1 + " 和 " + Num2 + "的乘积为  " + Result;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
