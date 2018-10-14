2f9ce7976b2c4b1f3350ba69cedbdc1788c15b05
ChCh1999 <34148951+ChCh1999@users.noreply.github.com>
2018-10-14 13:11:16 +0800

    homework5
    version 1

---
+++ HomeWork5/HomeWork5.2/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace HomeWork5._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textLength = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textThLeft = new System.Windows.Forms.TextBox();
+            this.textThRight = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Feedback = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textLength
+            // 
+            this.textLength.Location = new System.Drawing.Point(1164, 19);
+            this.textLength.Name = "textLength";
+            this.textLength.Size = new System.Drawing.Size(100, 25);
+            this.textLength.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("汉仪楷体繁", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(1029, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "根长度";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("汉仪楷体繁", 18F);
+            this.label2.Location = new System.Drawing.Point(1029, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "右偏角";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("汉仪楷体繁", 18F);
+            this.label3.Location = new System.Drawing.Point(1029, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "左偏角";
+            // 
+            // textThLeft
+            // 
+            this.textThLeft.Location = new System.Drawing.Point(1164, 60);
+            this.textThLeft.Name = "textThLeft";
+            this.textThLeft.Size = new System.Drawing.Size(100, 25);
+            this.textThLeft.TabIndex = 4;
+            // 
+            // textThRight
+            // 
+            this.textThRight.Location = new System.Drawing.Point(1164, 102);
+            this.textThRight.Name = "textThRight";
+            this.textThRight.Size = new System.Drawing.Size(100, 25);
+            this.textThRight.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.button1.Font = new System.Drawing.Font("汉仪楷体繁", 18F, System.Drawing.FontStyle.Italic);
+            this.button1.Location = new System.Drawing.Point(1100, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 42);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "绘制";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Feedback
+            // 
+            this.Feedback.AutoSize = true;
+            this.Feedback.Font = new System.Drawing.Font("汉仪楷体繁", 15F);
+            this.Feedback.ForeColor = System.Drawing.Color.Crimson;
+            this.Feedback.Location = new System.Drawing.Point(1029, 432);
+            this.Feedback.MaximumSize = new System.Drawing.Size(200, 0);
+            this.Feedback.Name = "Feedback";
+            this.Feedback.Size = new System.Drawing.Size(38, 25);
+            this.Feedback.TabIndex = 7;
+            this.Feedback.Text = "：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1276, 721);
+            this.Controls.Add(this.Feedback);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textThRight);
+            this.Controls.Add(this.textThLeft);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textLength);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Y型树";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textLength;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textThLeft;
+        private System.Windows.Forms.TextBox textThRight;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Feedback;
+    }
+}
+
